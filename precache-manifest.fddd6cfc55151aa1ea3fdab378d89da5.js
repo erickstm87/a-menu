5c820b014304c4c1ca91a953890467cc5ca49e1c
erickstm87 <dev@terickson.io>
2021-08-23 18:40:49 -0600

    Updates

---
+++ precache-manifest.fddd6cfc55151aa1ea3fdab378d89da5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c03fd7113d26203d455",
+    "url": "/a-menu/static/css/main.3dac3f7c.chunk.css"
+  },
+  {
+    "revision": "3c03fd7113d26203d455",
+    "url": "/a-menu/static/js/main.3c03fd71.chunk.js"
+  },
+  {
+    "revision": "5560d04d558a8920acef",
+    "url": "/a-menu/static/js/1.5560d04d.chunk.js"
+  },
+  {
+    "revision": "45dcd2701b8f2132664e",
+    "url": "/a-menu/static/js/runtime~main.45dcd270.js"
+  },
+  {
+    "revision": "54dd185a0004e9e25b649ef01739adfc",
+    "url": "/a-menu/index.html"
+  }
+];
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Appetizers from './Appetizers';
-import Note from './Notes';
-import PaymentForm from './PaymentForm';
-import Tables from './Tables';
-import Router from './Router';
-import Name from './Name';
-import SMSForm from './SMSForm';
-
-export {Appetizers, Note, PaymentForm, Tables, Router, Name, SMSForm}
