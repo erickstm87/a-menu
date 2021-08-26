fed67b15e23628f6196d9d3c429ece4b3beecd61
erickstm87 <dev@terickson.io>
2021-08-26 16:39:05 -0600

    Updates

---
+++ precache-manifest.83ed693bf6bc0dcc49d5e4f27555eef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5f0ced6a21e7fdced10",
+    "url": "/a-menu/static/css/main.3dac3f7c.chunk.css"
+  },
+  {
+    "revision": "f5f0ced6a21e7fdced10",
+    "url": "/a-menu/static/js/main.f5f0ced6.chunk.js"
+  },
+  {
+    "revision": "9c739d03417e4e993c43",
+    "url": "/a-menu/static/js/1.9c739d03.chunk.js"
+  },
+  {
+    "revision": "45dcd2701b8f2132664e",
+    "url": "/a-menu/static/js/runtime~main.45dcd270.js"
+  },
+  {
+    "revision": "b337dd342fd6d20a70cddca54c492c2a",
+    "url": "/a-menu/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/a-menu/precache-manifest.fddd6cfc55151aa1ea3fdab378d89da5.js"
+  "/a-menu/precache-manifest.83ed693bf6bc0dcc49d5e4f27555eef8.js"
 );
 
 workbox.clientsClaim();
